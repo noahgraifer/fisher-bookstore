3abba6375d030398f0bb58c5bbf959db402e49be
noahgraifer <46544235+noahgraifer@users.noreply.github.com>
2020-02-05 22:15:13 -0500

    Navigation Feature update (#17)
    * Added navigation bar changes
    
    * updated navigation feature

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import "./App.css";
 import {Navbar} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
+import Routes from "./Routes";
+
 export default class App extends Component {
   render() {
     return (
       <div className="App container">
-        <Navbar bg="dark" expand="lg">
+        <Navbar bg="light" expand="lg">
+
+        <Navbar bg="light" expand="lg">
+
           <Navbar.Brand>
             <Link to="/">Fisher Bookstore</Link>
           </Navbar.Brand>
         </Navbar> 
+
+        <Routes />
+
+
       </div>
     );
   }
