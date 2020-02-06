13883925e91655eefa3afcb24671da3aa773c2ff
Noah Graifer <ngraifer15@gmail.com>
2020-02-06 00:21:58 -0500

    Added Books Component and Nav

---
+++ src/books/Books.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Books.css";
+
+export default function Books() {
+    return(
+        <div className='Book'>
+            <div className="lander">
+                <h1>Books</h1>
+                <p>Bookstore Books</p>
+            </div>
+        </div>
+    );  
+}
