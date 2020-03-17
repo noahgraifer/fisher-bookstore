41d1c864bb133e705abb0fcdfb3b01aa5608db6b
Noah Graifer <ngraifer15@gmail.com>
2020-03-17 16:07:37 -0400

    Added Api call to Authors

--- src/authors/Authors.js
+++ src/authors/Authors.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import AuthorsDisplay from "./AuthorsDisplay";
 
-export default function Authors() {
+export default function Authors(props) {
     const [data, setData] = useState([]);
 
     useEffect(() => {
