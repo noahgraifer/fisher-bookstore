2664223fca25c79654ef3e6b8a5a1b7a40bea031
noahgraifer <46544235+noahgraifer@users.noreply.github.com>
2020-03-17 16:00:07 -0400

    Update AuthorsDisplay.js

--- src/authors/AuthorsDisplay.js
+++ src/authors/AuthorsDisplay.js
@@ -9,7 +9,7 @@ export default function AuthorsDisplay(props) {
             <h3>Authors</h3>
             <BootstrapTable data = { props.author } bordered  = { true } striped hover condensed>
                 <TableHeaderColumn dataField='id' isKey={ true }>Author Id</TableHeaderColumn>
-                <TableHeaderColumn dataField="name">First Name</TableHeaderColumn>
+                <TableHeaderColumn dataField="name">Name</TableHeaderColumn>
                 <TableHeaderColumn dataField="books">Last Name</TableHeaderColumn>
             </BootstrapTable>
         </div>
