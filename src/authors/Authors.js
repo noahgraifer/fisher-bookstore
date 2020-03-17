09a8232346e2269ebf13b5e5ca9a599bd05ff198
Noah Graifer <ngraifer15@gmail.com>
2020-03-17 16:05:34 -0400

    Added API call to Authors

--- src/authors/Authors.js
+++ src/authors/Authors.js
@@ -1,62 +1,20 @@
-import React, { Component } from "react";
-import "./Authors.css";
+import React, { useEffect, useState } from "react";
 import AuthorsDisplay from "./AuthorsDisplay";
 
-export default class Authors extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            authors: [
-                {
-                    id: 1,
-                    firstName: "Eric",
-                    lastName: "Evans"
-                },
-                {
-                    id: 2,
-                    firstName: "Nicole",
-                    lastName: "Forsgren"
-                },
-                {
-                    id: 3,
-                    firstName: "David",
-                    lastName: "Enrich"
-                },
-                {
-                    id: 4,
-                    firstName: "Suzanne",
-                    lastName: "Collins"
-                },
-                {
-                    id: 5,
-                    firstName: "Jeanine",
-                    lastName: "Cummins"
-                },
-                {
-                    id: 6,
-                    firstName: "Delia",
-                    lastName: "Owens"
-                },
-                {
-                    id: 7,
-                    firstName: "Jerry",
-                    lastName: "Craft"
-                },
-                {
-                    id: 8,
-                    firstName: "Gordon",
-                    lastName: "Korman"
-                }
-            ]
-        }
-    }
-    render() {
-        return(
-            <div className='Authors'>
-                <div className="lander">
-                    <AuthorsDisplay author={this.state.authors} />
-                </div>
+export default function Authors() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch("https://localhost:5001/api/authors/")
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, []);
+
+    return(
+        <div className='Authors'>
+            <div className="lander">
+                <AuthorsDisplay author={data} />
             </div>
-        );
-    }
+        </div>
+    );
 }
--- src/authors/AuthorsDisplay.js
+++ src/authors/AuthorsDisplay.js
@@ -9,8 +9,8 @@ export default function AuthorsDisplay(props) {
             <h3>Authors</h3>
             <BootstrapTable data = { props.author } bordered  = { true } striped hover condensed>
                 <TableHeaderColumn dataField='id' isKey={ true }>Author Id</TableHeaderColumn>
-                <TableHeaderColumn dataField="firstName">First Name</TableHeaderColumn>
-                <TableHeaderColumn dataField="lastName">Last Name</TableHeaderColumn> 
+                <TableHeaderColumn dataField="name">First Name</TableHeaderColumn>
+                <TableHeaderColumn dataField="books">Last Name</TableHeaderColumn> 
             </BootstrapTable>
         </div>
     );
